41e55996acf83fb068c34b74b3cda90fb69d195b
Freifunk Troisdorf <root@ansible.freifunk-rhein-sieg.net>
2019-02-05 21:40:58 +0100

    Housekeeping

--- files/check_mk-dhcp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-datum=$(date "+%b %d")
-hostname=$(hostname)
-clients=$(cat /var/log/syslog | grep "$(date "+%b %d")" | grep DHCPACK | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' | sort | uniq | wc -l)
-echo "nc.gateways."$hostname" $clients `date +%s`" | nc -n -q 5 10.188.0.10 2003
-echo "0 Uniq-Clients count=$clients - $clients Uniq Clients heute"
--- files/start-broker-backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-WDIR=/srv/tunneldigger
-VIRTUALENV_DIR=/srv/tunneldigger
-
-cd $WDIR
-source $VIRTUALENV_DIR/bin/activate
-
-bin/python broker/l2tp_broker.py l2tp_broker-backup.cfg
